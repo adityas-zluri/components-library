a310e525dadfbc9111b9fcf5a6655a53c12e0c4d
Aditya Singh <aditya.singh@zluri.com>
2024-05-04 11:29:25 +0530

    disabled prop added to Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,14 +3,15 @@ import React from "react";
 interface ButtonProps {
   label: string;
   style: object;
+  disabled: boolean;
   onClick: () => void;
 }
 
 export default function Button(props: ButtonProps) {
-  const { label, onClick, style } = props;
+  const { label, onClick, style, disabled } = props;
   return (
     <>
-      <button onClick={onClick} style={style}>
+      <button onClick={onClick} style={style} disabled={disabled}>
         {label}
       </button>
     </>
